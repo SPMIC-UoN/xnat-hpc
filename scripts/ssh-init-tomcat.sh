d44306b7d7ba57f97847e29d4a1a0b8de1a781bd
Martin Craig <martin.craig@nottingham.ac.uk>
2021-10-21 10:50:58 +0100

    Move hardcoded paths etc into variables at start of scripts

--- scripts/ssh-init-tomcat.sh
+++ scripts/ssh-init-tomcat.sh
@@ -9,10 +9,10 @@
 # It will prompt for the passphrase of the authentication key
 
 XNAT_HOME=/home/xnat
-XNAT_SSH_KEY=$XNAT_HOME/.ssh/id_rsa
+XNAT_SSH_KEY=$XNAT_HOME/.ssh/id_rsa_hpc
+XNAT_SSH_AGENT_ENVFILE=${XNAT_HOME}/.ssh/ssh-agent-tomcat-env
 
-ssh-agent >$XNAT_HOME/bin/ssh-agent-tomcat-env
-. $XNAT_HOME/bin/ssh-agent-tomcat-env
+ssh-agent >${XNAT_SSH_AGENT_ENVFILE}
+. ${XNAT_SSH_AGENT_ENVFILE}
 ssh-add $XNAT_SSH_KEY
-
 
