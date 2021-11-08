69d4db36ed88104003130c4c5451070031ec5b99
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-11-08 16:19:44 +0000

    Use base name of input files to form output file names

--- docker/ukat/ukat_t2star.py
+++ docker/ukat/ukat_t2star.py
@@ -20,11 +20,11 @@ outdir = sys.argv[2]
 print("Loading from %s" % indir)
 data = []
 affine = None
+fprefix = None
 for fname in os.listdir(indir):
     if fname.endswith(".nii") or fname.endswith(".nii.gz"):
         base_fname = fname[:fname.index(".")]
         if os.path.exists(os.path.join(indir, base_fname + ".json")):
-            print("Processing %s" % fname)
             nii = nib.load(os.path.join(indir, fname))
             with open(os.path.join(indir, base_fname + ".json")) as f:
                 metadata = json.load(f)
@@ -38,9 +38,14 @@ for fname in os.listdir(indir):
             elif "PHASE" in metadata.get("ImageType", []):
                 print("INFO: Image %s is phase - ignoring" % fname)
             else:
+                print("Processing %s" % fname)
                 data.append((te*1000, image))
                 if affine is None:
                     affine = nii.header.get_best_affine()
+                if fprefix is None:
+                    fprefix = base_fname
+                else:
+                    fprefix = os.path.commonprefix([fprefix, base_fname])
         else:
             print("WARNING: Found Nifti file %s without corresponding JSON - ignoring" % fname)
 
@@ -55,7 +60,7 @@ if data:
     t2star_loglin = mapper_loglin.t2star_map
 
     # Save output maps to Nifti
-    mapper_loglin.to_nifti(output_directory=outdir, maps=['m0', 't2star'], base_file_name="ukat")
+    mapper_loglin.to_nifti(output_directory=outdir, maps=['m0', 't2star'], base_file_name=fprefix)
 else:
     print("WARNING: No data found - no T2* map will be generated")
 
