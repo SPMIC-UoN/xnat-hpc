453fd2d7edd223b860af49f7becfce86e07326cd
Martin Craig <martin.craig@nottingham.ac.uk>
2021-11-16 13:51:49 +0000

    Bump version

--- docker/ukat/build.sh
+++ docker/ukat/build.sh
@@ -3,7 +3,7 @@
 cp Dockerfile.in Dockerfile
 python ../cmd2label.py ukat_t2star_cmd.json ukat_b0_cmd.json >> Dockerfile
 
-tag=0.0.2
+tag=0.0.3
 docker build -t martincraig/xnat-ukat .
 docker tag martincraig/xnat-ukat martincraig/xnat-ukat:$tag 
 docker push martincraig/xnat-ukat:$tag
