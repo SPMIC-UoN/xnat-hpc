ea04b80022036e26b17d572e7a4c623c0a629e8d
Martin Craig <martin.craig@nottingham.ac.uk>
2021-11-17 14:36:37 +0000

    Send log output to working directory log files

--- scripts/hpc/slurm/mriqc.sh
+++ scripts/hpc/slurm/mriqc.sh
@@ -4,16 +4,15 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32gb
 #SBATCH --time=04:00:00
-#SBATCH -e error_%A_%a.log
-#SBATCH -o output_%A_%a.log
 #SBATCH --job-name=mriqc
 #SBATCH --partition=imgcomputeq
 #SBATCH --qos=img
 
 SIMG=/software/imaging/singularity_images/poldracklab_mriqc-2021-01-30-767af1135fae.simg
 INDIR=$1
-OUTDIR=$2
+WORKDIR=$2
+OUT_SUBDIR=$3
 
 module load singularity
-singularity run --cleanenv $SIMG $INDIR $OUTDIR participant
+singularity run --cleanenv $SIMG $INDIR $WORKDIR/$OUT_SUBDIR participant --no-sub >>$WORKDIR/out.log 2>>$WORKDIR/err.log
 
