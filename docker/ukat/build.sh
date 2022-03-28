24fd9c8e16ef17a085eae9314817bb778b7481f7
Martin Craig <martin.craig@nottingham.ac.uk>
2022-03-28 10:35:36 +0100

    ukat code moved to separate module

--- docker/ukat/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile.in Dockerfile
-python ../cmd2label.py ukat_t2star_cmd.json ukat_b0_cmd.json >> Dockerfile
-
-tag=0.0.3
-docker build -t martincraig/xnat-ukat .
-docker tag martincraig/xnat-ukat martincraig/xnat-ukat:$tag 
-docker push martincraig/xnat-ukat:$tag
