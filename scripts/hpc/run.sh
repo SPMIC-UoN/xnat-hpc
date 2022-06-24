6acb0fa651a458ea6b4aa6400ac3ad40383428e8
Martin Craig <martin.craig@nottingham.ac.uk>
2022-06-24 13:57:27 +0100

    Add wrapper script

---
+++ scripts/hpc/run.sh
@@ -0,0 +1,17 @@
+#Wrapper script to run named command
+#
+# This is very simple at present but we keep it so we
+# have a level of indirection that can be used in the
+# future for better error handling etc and which doesn't
+# require changes on the XNAT instance
+HPC_COMMAND_DIR=$HOME/code
+
+CMD=$1
+HOST=$2
+JSESSION=$3
+PROJ=$4
+SUBJ=$5
+EXP=$6
+ARGS=$7
+CURL_CA_BUNDLE="" ${HPC_COMMAND_DIR}/${CMD}/run "$HOST" "$JSESSION" "$PROJ" "$SUBJ" "$EXP" $ARGS
+
