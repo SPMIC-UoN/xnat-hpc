1ad4d7a766b61d726de7c5713d8feeb038cd5427
Martin Craig <martin.craig@nottingham.ac.uk>
2022-06-24 14:23:33 +0100

    Convert wrapper to python with basic logging and error checking

--- scripts/hpc/run.sh
+++ scripts/hpc/run.sh
@@ -1,17 +1,38 @@
-#Wrapper script to run named command
+#!/bin/env python
 #
-# This is very simple at present but we keep it so we
-# have a level of indirection that can be used in the
-# future for better error handling etc and which doesn't
-# require changes on the XNAT instance
-HPC_COMMAND_DIR=$HOME/xnat-hpc/scripts/hpc/hpc-commands
+# Wrapper script to run named command
+#
+# For historical reasons this is .sh but is now a python script
 
-CMD=$1
-HOST=$2
-JSESSION=$3
-PROJ=$4
-SUBJ=$5
-EXP=$6
-ARGS=$7
-CURL_CA_BUNDLE="" ${HPC_COMMAND_DIR}/${CMD}/run "$HOST" "$JSESSION" "$PROJ" "$SUBJ" "$EXP" $ARGS
+import os
+import sys
+import logging
+
+LOG = logging.getLogger("XNAT_HPC")
+LOG_LEVEL = logging.INFO
+HPC_COMMAND_DIR = "hpc-commands"
+
+# Send the log to an output logfile
+logging.basicConfig(filename="xnat_hpc.log", filemode="a", level=LOG_LEVEL)
+
+# Also to stdout
+extra_handler = logging.StreamHandler(sys.stdout)
+extra_handler.setFormatter(logging.Formatter('%(levelname)s : %(message)s'))
+LOG.addHandler(extra_handler)
+
+if len(sys.argv) != 8:
+    LOG.error("Called with incorrect number of arguments: %i (%s)", len(sys.argv), sys.argv)
+    sys.exit(1)
+
+cmd, host, jsessionid, proj, subj, exp, args = sys.argv[1:]
+cmd = os.path.join(HPC_COMMAND_DIR, cmd, "run")
+if not os.path.isfile(cmd):
+    LOG.error("No such command: %s", cmd)
+else:
+    os.environ["CURL_CA_BUNDLE"] = ""
+    fullcmd = cmd + f' "{host}" "{jsessionid}" "{proj}" "{subj}" "{exp}" {args}'
+    LOG.info(fullcmd)
+    retval = os.system(fullcmd)
+    if retval != 0:
+        LOG.error("Exit status: %i: %s", retval, fullcmd)
 
